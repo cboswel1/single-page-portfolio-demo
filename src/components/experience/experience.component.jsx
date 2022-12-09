71b79284b2e2dcd34319b3937cb5ed0d4d893a6f
Christian Boswell <christian.boswell@thinkingbox.com>
2022-12-09 16:03:13 -0700

    experience mobile framing

--- src/components/experience/experience.component.jsx
+++ src/components/experience/experience.component.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import './experience.styles.scss'
+import './experience.styles.scss';
 
 const Experience = () => {
+  const experience = [
+    { id: 1, name: 'HTML', time: '4 Years of Experience' },
+    { id: 2, name: 'CSS', time: '4 Years of Experience' },
+    { id: 3, name: 'Javascript', time: '4 Years of Experience' },
+    { id: 4, name: 'Accessibility', time: '4 Years of Experience' },
+    { id: 5, name: 'React', time: '3 Years of Experience' },
+    { id: 5, name: 'Sass', time: '3 Years of Experience' },
+  ];
 
-    return (
-        <h1 className="language">Experience</h1>
-    )
-}
+  return (
+    <div className="experience">
+      <div className="experience__wrapper">
+        {experience.map((experience) => {
+          return (
+            <div className="experience__content" key={experience.id}>
+              <h2 className="experience__title">{experience.name}</h2>
+              <h3>{experience.time}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Experience
+export default Experience;
